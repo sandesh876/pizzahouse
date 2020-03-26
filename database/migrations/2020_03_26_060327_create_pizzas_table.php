b7d9859564037533b7c80c24dca5dc65d62331b8
Sandesh Gautam <sandeshgautam876@gmail.com>
2020-03-26 13:06:54 +0545

    first commit

---
+++ database/migrations/2020_03_26_060327_create_pizzas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePizzasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizzas',function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('type');
+            $table->string('base');
+            $table->string('name');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizzas');
+        
+    }
+}
